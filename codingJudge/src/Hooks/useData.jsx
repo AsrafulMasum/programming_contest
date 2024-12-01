ded6289467763927767bb200e7a084da6549a0cc
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2024-12-02 04:04:38 +0600

    irrelevent hooks deleted

--- codingJudge/src/Hooks/useData.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { DataContext } from "../Provider/DataProvider";
-
-const useData = () => {
-  const data = useContext(DataContext);
-  return data;
-};
-
-export default useData;
