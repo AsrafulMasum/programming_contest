5f8bbac9e1811cebdb8a6768c9826c85f1c59186
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2024-12-04 03:03:56 +0600

    error solved

--- codingJudge/src/main.jsx
+++ codingJudge/src/main.jsx
@@ -4,22 +4,19 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import Routes from "./Routes/Routes.jsx";
 import AuthProvider from "./Provider/AuthProvider";
-import DataProvider from "./Provider/DataProvider";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClint = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClint}>
-      <DataProvider>
-        <AuthProvider>
-          <RouterProvider router={Routes}></RouterProvider>
-        </AuthProvider>
-      </DataProvider>
+      <AuthProvider>
+        <RouterProvider router={Routes}></RouterProvider>
+      </AuthProvider>
     </QueryClientProvider>
     <Toaster />
     <ToastContainer />
